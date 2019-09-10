3f2684772a7156f333a9a173e7cd17536b51a807
Tassia Castro <tassiaserrao@gmail.com>
2019-09-10 21:45:15 +0200

    Create UserSerializer

--- todos/serializers.py
+++ todos/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from .models import Todo
+from django.contrib.auth.models import User
 
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ('message', 'completed')
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
