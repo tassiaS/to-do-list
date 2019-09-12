808525524e5b03b66ffbcf2f0225dff166aafd2f
Tassia Castro <tassiaserrao@gmail.com>
2019-09-12 09:28:47 +0200

    Add Todo id to serializer

--- todos/serializers.py
+++ todos/serializers.py
@@ -6,10 +6,5 @@ from django.contrib.auth.models import User
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ('message', 'completed')
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        extra_kwargs = {'password': {'write_only': True}}
-        fields = ('id', 'first_name', 'last_name', 'email', 'password')
+        fields = ('id', 'message', 'completed')
+        extra_kwargs = {'id': {'read_only': True}}
