f898803813bb7a49eb17cf190d7a108aeb9c2771
Tassia Castro <tassiaserrao@gmail.com>
2019-09-12 09:29:18 +0200

    Update tests to include authentication

--- todos/tests/test_views.py
+++ todos/tests/test_views.py
@@ -4,18 +4,36 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from ..models import Todo
 from ..serializers import TodoSerializer
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
 
 
 # initialize the APIClient app
-client = Client()
+# user = User.objects.get(username='lauren')
+client = APIClient()
+# client.force_authenticate(user=user)
 
 class getAllTodosTests(TestCase):
     "Test module for get all todos API"
 
     def setUp(self):
-        Todo.objects.create(message="Buy milk")
-        Todo.objects.create(message="Buy rice")
-        Todo.objects.create(message="Buy chocolate")
+        user = User.objects.create_user('username', 'email', 'password')
+        token = Token.objects.get(user=user)
+        client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        # self.username = 'askerTest'
+        # self.email = 'askerTest@gmail.com'
+        # self.user = User.objects.create_user('username', 'email', 'password')
+        # self.token = Token.objects.get(user=user)
+        # self.api_authentication()
+        
+        Todo.objects.create(message="Buy milk", user=self.user)
+        Todo.objects.create(message="Buy rice", user=self.user)
+        Todo.objects.create(message="Buy chocolate", user=self.user)
+   
+    # def api_authentication(self):
+    #     self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
     def test_get_all_todos(self):
         #get API response
@@ -31,6 +49,12 @@ class CreateTodoTest(TestCase):
     "Test module for inserting a new todo"
 
     def setUp(self):
+        # self.username = 'askerTest'
+        # self.email = 'askerTest@gmail.com'
+        # self.user = User.objects.create_user(self.username, self.email, 'password')
+        # self.token = Token.objects.get(user__username='askerTest')
+        # self.api_authentication()
+        
         self.valid_todo = {
             'message':'Buy milk'
         }
@@ -38,6 +62,9 @@ class CreateTodoTest(TestCase):
         self.invalid_todo = {
             'message':''
         }
+    
+    # def api_authentication(self):
+    #     self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
     
     def test_create_valid_todo(self):
         response =  client.post(reverse('get_post_todo'), 
@@ -58,6 +85,12 @@ class UpdateTodoTest(TestCase):
     "Test module for updating a todo"
 
     def setUp(self):
+        # self.username = 'askerTest'
+        # self.email = 'askerTest@gmail.com'
+        # self.user = User.objects.create_user(self.username, self.email, 'password')
+        # self.token = Token.objects.get(user__username='askerTest')
+        # self.api_authentication()
+
         self.todo_chocolate = Todo.objects.create(message='Buy Chocolate')
         self.valid_todo = {
             'message':'Buy milk'
@@ -66,6 +99,9 @@ class UpdateTodoTest(TestCase):
         self.invalid_todo = {
             'message':''
         }
+    
+    # def api_authentication(self):
+    #     self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
     
     def test_update_valid_todo(self):
         response = client.put(reverse('delete_update_todo', 
@@ -89,7 +125,16 @@ class deleteTodoTest(TestCase):
     "Test module for deleting a todo"
 
     def setUp(self):
+        # self.username = 'askerTest'
+        # self.email = 'askerTest@gmail.com'
+        # self.user = User.objects.create_user(self.username, self.email, 'password')
+        # self.token = Token.objects.get(user__username='askerTest')
+        # self.api_authentication()
+
         self.todo_chocolate = Todo.objects.create(message='Buy chocolate')
+
+    # def api_authentication(self):
+    #     self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
     
     def test_delete_valid_todo(self):
         response = client.delete(reverse('delete_update_todo', kwargs={'pk': self.todo_chocolate.pk}))
--- todos/views.py
+++ todos/views.py
@@ -13,23 +13,20 @@ def delete_update_todo(request, pk):
     except Todo.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
+    if todo.user != request.user :
+        return Response(status=status.HTTP_403_FORBIDDEN)
+
     # delete a single todo
     if request.method == 'DELETE':
-        if todo.user == request.user :
-            todo.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
     #  update of a single todo
     elif request.method == 'PUT':
-        if todo.user == request.user:
-            serializer = TodoSerializer(todo, data = request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status= status.HTTP_204_NO_CONTENT)
-        else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
+        serializer = TodoSerializer(todo, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_204_NO_CONTENT)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -37,16 +34,13 @@ def delete_update_todo(request, pk):
 def get_post_todo(request):
     # get all todos
     if request.method == 'GET':
-        todos = Todo.objects.all()
+        todos = Todo.objects.filter(user=request.user)
         serializer = TodoSerializer(todos, many=True)
         return Response(serializer.data)
     
     # insert a new record for a todo
     elif request.method == 'POST':
-        data = {
-            'message': request.data.get('message')
-        }
-        serializer = TodoSerializer(data=data)
+        serializer = TodoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(user=request.user)
             return Response(serializer.data, status = status.HTTP_201_CREATED)
